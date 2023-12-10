a1305452cfc41b9e831c486dd4fc9a3ae243bbcf
rahmathidayatullah <rahmathidayatullah996@gmail.com>
2023-12-10 11:57:34 +0700

    inject service to service

--- services-start/src/app/account/account.component.ts
+++ services-start/src/app/account/account.component.ts
@@ -6,23 +6,19 @@ import { LoggingService } from '../logging.service';
   selector: 'app-account',
   templateUrl: './account.component.html',
   styleUrls: ['./account.component.css'],
-  providers: [LoggingService],
 })
 export class AccountComponent {
   @Input() account: { name: string; status: string };
   @Input() id: number;
+
   @Output() statusChanged = new EventEmitter<{
     id: number;
     newStatus: string;
   }>();
 
-  constructor(
-    private loggingService: LoggingService,
-    private accountService: AccountService
-  ) {}
+  constructor(private accountService: AccountService) {}
 
   onSetTo(status: string) {
     this.accountService.updateStatus(this.id, status);
-    this.loggingService.logStatusChange(status);
   }
 }
--- services-start/src/app/app.module.ts
+++ services-start/src/app/app.module.ts
@@ -2,22 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
-
 import { AppComponent } from './app.component';
 import { AccountComponent } from './account/account.component';
 import { NewAccountComponent } from './new-account/new-account.component';
+import { LoggingService } from './logging.service';
+import { AccountService } from './accounts.service';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    AccountComponent,
-    NewAccountComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent, AccountComponent, NewAccountComponent],
+  imports: [BrowserModule, FormsModule],
+  providers: [AccountService, LoggingService],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
