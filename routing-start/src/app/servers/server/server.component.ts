8e5ce216d392feec4a341d6c113f36c02e3b973a
rahmattSera <Rahmat.Hidayatullah@sera.astra.co.id>
2024-01-14 17:17:41 +0700

    routing : add query params and handle retrieving params with queryparams map

--- routing-start/src/app/servers/server/server.component.ts
+++ routing-start/src/app/servers/server/server.component.ts
@@ -13,17 +13,23 @@ export class ServerComponent implements OnInit {
 
   constructor(
     private serversService: ServersService,
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private router: Router
   ) {}
 
   ngOnInit() {
     const id = +this.route.snapshot.params['id'];
-    console.log('id', id);
 
     this.server = this.serversService.getServer(id);
-    // console.log('this.server', this.server);
     this.route.params.subscribe((params: Params) => {
       this.server = this.serversService.getServer(+params['id']);
+    });
+  }
+
+  onEdit() {
+    this.router.navigate(['edit'], {
+      relativeTo: this.route,
+      queryParamsHandling: 'preserve',
     });
   }
 }
