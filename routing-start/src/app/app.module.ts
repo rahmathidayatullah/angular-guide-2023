93ec4e5bd55f4c2e67344231e7d867c19a10d284
rahmattSera <Rahmat.Hidayatullah@sera.astra.co.id>
2024-01-14 17:26:20 +0700

    routing : page not found handle app route module

--- routing-start/src/app/app.module.ts
+++ routing-start/src/app/app.module.ts
@@ -11,6 +11,7 @@ import { EditServerComponent } from './servers/edit-server/edit-server.component
 import { ServerComponent } from './servers/server/server.component';
 import { ServersService } from './servers/servers.service';
 import { RouterModule, Routes } from '@angular/router';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 const appRoutes: Routes = [
   {
@@ -41,6 +42,18 @@ const appRoutes: Routes = [
       },
     ],
   },
+  {
+    path: 'not-found',
+    component: PageNotFoundComponent,
+  },
+  {
+    path: '**',
+    redirectTo: '/not-found',
+  },
+  // {
+  //   path: 'something-page',
+  //   redirectTo: '/not-found',
+  // },
 ];
 
 @NgModule({
