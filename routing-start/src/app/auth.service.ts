36782c6402c2277f51df6b8b9f6a544aef903c0a
rahmattSera <Rahmat.Hidayatullah@sera.astra.co.id>
2024-01-14 20:48:25 +0700

    routing : login logout fake for testing canActive Router

--- routing-start/src/app/auth.service.ts
+++ routing-start/src/app/auth.service.ts
@@ -7,14 +7,17 @@ export class AuthService {
         resolve(this.loggedIn);
       }, 800);
     });
+    console.log('isAuthenticated', this.loggedIn);
     return promise;
   }
 
   login() {
     this.loggedIn = true;
+    console.log('login', this.loggedIn);
   }
 
   logout() {
     this.loggedIn = false;
+    console.log('logout', this.loggedIn);
   }
 }
