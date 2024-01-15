c6b1016af5faab3b21b129f1ab72fb9e47c440fb
rahmattSera <Rahmat.Hidayatullah@sera.astra.co.id>
2024-01-15 11:40:57 +0700

    routing : resolver dynamic utils for handle params id

--- routing-start/src/app/app-routing.module.ts
+++ routing-start/src/app/app-routing.module.ts
@@ -11,6 +11,7 @@ import { PageNotFoundComponent } from './page-not-found/page-not-found.component
 import { AuthGuard } from './auth-gurad.service';
 import { CanDeactivateGuard } from './servers/edit-server/can-deactivate-guard.service';
 import { ErrorPageComponent } from './error-page/error-page.component';
+import { ServerResolver } from './servers/server/server-resolver.service';
 
 const appRoutes: Routes = [
   {
@@ -36,6 +37,7 @@ const appRoutes: Routes = [
       {
         path: ':id',
         component: ServerComponent,
+        resolve: { server: ServerResolver },
       },
       {
         path: ':id/edit',
