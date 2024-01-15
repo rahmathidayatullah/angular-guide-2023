67fdbb6948710b573f9d4e762d5bb11a36531c1c
rahmattSera <Rahmat.Hidayatullah@sera.astra.co.id>
2024-01-15 11:27:27 +0700

    routing : error page and passing data from route

--- routing-start/src/app/app-routing.module.ts
+++ routing-start/src/app/app-routing.module.ts
@@ -10,6 +10,7 @@ import { ServerComponent } from './servers/server/server.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { AuthGuard } from './auth-gurad.service';
 import { CanDeactivateGuard } from './servers/edit-server/can-deactivate-guard.service';
+import { ErrorPageComponent } from './error-page/error-page.component';
 
 const appRoutes: Routes = [
   {
@@ -43,18 +44,25 @@ const appRoutes: Routes = [
       },
     ],
   },
+  {
+    path: 'not-found',
+    component: ErrorPageComponent,
+    data: {
+      message: 'Page not found!',
+    },
+  },
   // {
   //   path: 'not-found',
   //   component: PageNotFoundComponent,
   // },
-  // {
-  //   path: '**',
-  //   redirectTo: '/not-found',
-  // },
   {
     path: '**',
-    component: PageNotFoundComponent,
+    redirectTo: '/not-found',
   },
+  // {
+  //   path: '**',
+  //   component: PageNotFoundComponent,
+  // },
   // {
   //   path: 'something-page',
   //   redirectTo: '/not-found',
